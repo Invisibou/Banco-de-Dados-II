fb885ca8dda8f3644a5eb5b3542a2b59baf0b976
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2025-08-08 22:19:48 -0300

    Aula-Banco-De-Dados-II

---
+++ EFTeste/EFTeste/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EFTeste.Models;
+
+namespace EFTeste.Data
+{
+	public class SchoolContext : DbContext
+	{
+		public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) 
+		{
+
+		}
+
+		public DbSet<Student> Students { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Student>().ToTable("Student");
+		}
+	}
+}
---
+++ EFTeste/EFTeste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFTeste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
